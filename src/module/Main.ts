9b191e17eebf38e6f100e2c6fdbc33def6dcc7ed
Andy <andrewcuccinello@gmail.com>
2020-09-07 16:41:03 -0400

    Fix debug opening of sheet left in.

--- src/module/Main.ts
+++ src/module/Main.ts
@@ -71,9 +71,9 @@ Hooks.on('setup', () => {
         }
     });
 
-    Hooks.on('ready', () => {
-        setTimeout(() => (game.actors.getName('Loot') as Actor).sheet.render(true), 500);
-    });
+    // Hooks.on('ready', () => {
+    //     setTimeout(() => (game.actors.getName('Loot') as Actor).sheet.render(true), 500);
+    // });
 });
 
 function onScaleNPCContextHook(html: JQuery, buttons: any[]) {
